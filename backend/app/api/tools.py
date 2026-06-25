4f381675ac1cbe565645250818a2bc7666ce8b0c
zakariaelkahri <zakariakahri2003@gmail.com>
2026-06-25 15:58:14 +0100

    agent basic v1 ready backend

--- backend/app/api/tools.py
+++ backend/app/api/tools.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter 
 import requests
-
+from app.chatbot.agent import agent
 
 
 router = APIRouter(prefix="/chatbot", tags=["Chatbot"])
 
 
 
-
+@router.get("/agent")
+def get_agent_response(query: str):
+    """
+    Get a response from the chatbot agent based on the provided query.
+    """
+    response = agent.invoke({"input": query})
+    return {"response": response["output"]}
 
--- backend/app/chatbot/agent.py
+++ backend/app/chatbot/agent.py
@@ -9,9 +9,9 @@ binder = create_tool_calling_agent( llm, TOOLS, prompt)
 agent = AgentExecutor(
     agent=binder,
     tools=TOOLS,
-    verbose=True
+    # verbose=True
 )
 
-# test 
-# response  = agent.invoke({"input" : "What is the the relation between items, customers and sales orders?"})
+# # test 
+# response  = agent.invoke({"input" : "What is the the list of available items?"})
 # print(response["output"])
